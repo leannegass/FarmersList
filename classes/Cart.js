aa41224a6e95fa72d629c5b0345bbe7fef12201c
Leanne Gass <leannegass@gmail.com>
2023-04-24 18:20:11 +0100

    added getTotal method to Cart.js

--- classes/Cart.js
+++ classes/Cart.js
@@ -9,15 +9,16 @@ class Cart {
         this.products.push(product);
         this.total += product.price;
     }
-    removeProduct(product){
+    removeProduct(i){
         //console.log(this.products[product].price);
-        this.total -= this.products[product].price;
-        let i = this.products.indexOf(product);
+        this.total -= this.products[i].price;
+        //let i = this.products.indexOf(i);
         this.products.splice(i,1);
-        
-
-        
-
+    }
+    getTotal(){
+        let sum = 0;
+        this.products.forEach(element => { sum += element.price});
+        return sum;
     }
 }
 module.exports = Cart;
